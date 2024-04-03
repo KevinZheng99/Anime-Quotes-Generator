2401e591da3e83079cc636693d1a7f5d0ce0c68e
KevinZheng99 <kevinzhengkz99@gmail.com>
2024-04-02 23:54:27 -0400

    anime selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 import QuoteCard from "./components/QuoteCard";
+import { animeSelect } from "./const";
 
 function App() {
   const [animeQuotes, setAnimeQuotes] = useState();
+  const [selectedAnime, setSelectedAnime] = useState("");
 
   function handleGenerateQuote() {
-    fetch("https://animechan.xyz/api/quotes")
+    fetch(
+      `https://animechan.xyz/api/quotes${
+        selectedAnime ? "/anime?title=" + selectedAnime : ""
+      }`
+    )
       .then((response) => response.json())
       .then((quote) => setAnimeQuotes(quote));
+  }
+
+  function handleChange(event) {
+    setSelectedAnime(event.target.value);
   }
 
   return (
@@ -19,8 +33,25 @@ function App() {
         flexDirection: "column",
         gap: "12px",
         alignItems: "center",
+        marginTop: "12px",
       }}
     >
+      <FormControl sx={{ width: "275px", margin: "auto" }}>
+        <InputLabel id="Anime">Anime</InputLabel>
+        <Select
+          labelId="Anime"
+          id="anime"
+          value={selectedAnime}
+          label="Anime"
+          onChange={handleChange}
+        >
+          {animeSelect.map((anime) => (
+            <MenuItem key={anime} value={anime}>
+              {anime}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
       <Button
         sx={{ width: "275px" }}
         variant="contained"
---
+++ src/const/index.js
@@ -0,0 +1,1 @@
+export const animeSelect = ["Naruto", "Bleach", "One Piece"];
